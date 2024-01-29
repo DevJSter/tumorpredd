de51c3b3c148da6b61c31a2c6c32192bc7f6fdfe
Shubham Tiwari <shubht3303@gmail.com>
2024-01-29 16:47:52 +0530

    added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { MdCloudUpload, MdDelete } from "react-icons/md";
+import { storage } from "../firebase.config";
+import Loader from "../components/Loader";
 
-function App() {
+const TumorForm = () => {
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleImageChange = (e) => {
+    const url = e.target.value;
+    setImageUrl(url);
+  };
+
+  const [imageAsset, setImageAsset] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
+      (error) => {
+        console.log(error);
+        setFields(true);
+        setMsg("Error while uploading : Try Again!!!");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageAsset(downloadURL);
+          console.log(downloadURL);
+          setIsLoading(false);
+          setFields(true);
+          setMsg("Image uploaded successfully!!!");
+          setAlertStatus("success");
+          setTimeout(() => {
+            setFields(false);
+          }, 4000);
+        });
+      }
+    );
+  };
+
+  const deleteImage = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setFields(true);
+      setMsg("Image deleted successfully!!!");
+      setAlertStatus("success");
+      setTimeout(() => {
+        setFields(false);
+      }, 4000);
+    });
+  };
+
+  const handlePredictClick = async () => {
+    try {
+      setLoading(true);
+      console.log(imageAsset);
+
+      const response = await axios.post(
+        "/tumorpredict",
+        { url: imageAsset },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      setLoading(false);
+      console.log(response.data);
+      setResult(response.data.result.toString()); // Convert the object to a string
+    } catch (error) {
+      console.error("Error making prediction:", error);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card max-w-2xl mx-auto p-8">
+      <div className="text-center">
+        <h1 className="text-blue-500 text-2xl font-bold mb-4">
+          Prediction Form:
+        </h1>
+
+        <form className="flex flex-col items-center justify-center">
+          <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <>
+                {!imageAsset ? (
+                  <>
+                    <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                      <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                        <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                        <p className="text-gray-500 hover:text-gray-700">
+                          Click here to upload
+                        </p>
+                      </div>
+                      <input
+                        type="file"
+                        name="uploadimage"
+                        accept="image/*"
+                        onChange={uploadImage}
+                        className="w-0 h-0"
+                      />
+                    </label>
+                  </>
+                ) : (
+                  <>
+                    <div className="relative h-full">
+                      <img
+                        src={imageAsset}
+                        alt="uploaded image"
+                        className="w-full h-full object-cover"
+                      />
+                      <button
+                        type="button"
+                        className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md  duration-500 transition-all ease-in-out"
+                        onClick={deleteImage}
+                      >
+                        <MdDelete className="text-white" />
+                      </button>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+          {/* <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Image URL:
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                onChange={handleImageChange}
+              />
+            </label>
+          </div> */}
+          <button
+            className="bg-blue-500 w-full m-6 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={handlePredictClick}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      {loading && <p>Loading...</p>}
+      {result && (
+        <div className="mt-4">
+          <h2 className="tag">Result: {result}</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default TumorForm;
